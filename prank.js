906daf92c173c60788d24e97a2fd8dc1aac2a134
Mohtech <57531506+mohatrust@users.noreply.github.com>
2023-03-16 10:52:19 +0300

    Create prank.js

---
+++ prank.js
@@ -0,0 +1,205 @@
+/* This script should be used with caution kindly seek permission before using this script against anybody 
+# This should work on windows 10, 11 or later versions
+*/
+
+language="us";
+press("GUI r");
+delay(500)
+type("cmd");
+delay(500)
+type("color c");
+press("ENTER");
+delay(500)
+type("cls");
+press("ENTER");
+delay(400)
+type("tree");
+press("ENTER");
+delay(400)
+press("GUI r");
+delay(500)
+type("cmd");
+press("ENTER");
+delay(500)
+type("color a");
+press("ENTER");
+delay(500)
+type("START");
+press("ENTER");
+delay(500)
+press("GUI r");
+delay(500)
+type("cmd");
+press("ENTER");
+delay(500)
+type("color a");
+press("ENTER");
+delay(500)
+type("START");
+press("ENTER");
+delay(500)
+press("GUI r");
+delay(500)
+type("cmd");
+press("ENTER");
+delay(500)
+type("color a");
+press("ENTER");
+delay(500)
+type("START");
+press("ENTER");
+delay(500)
+press("GUI r");
+delay(500)
+type("cmd");
+press("ENTER");
+delay(500)
+type("color a");
+press("ENTER");
+delay(500)
+type("START");
+press("ENTER");
+delay(500)
+press("GUI r");
+delay(500)
+type("cmd");
+press("ENTER");
+delay(500)
+type("color a");
+press("ENTER");
+delay(500)
+type("START");
+press("ENTER");
+delay(500)
+press("GUI r");
+delay(500)
+type("cmd");
+press("ENTER");
+delay(500)
+type("color a");
+press("ENTER");
+delay(500)
+type("START");
+press("ENTER");
+delay(500)
+press("GUI r");
+delay(500)
+type("cmd");
+press("ENTER");
+delay(500)
+type("color a");
+press("ENTER");
+delay(500)
+type("START");
+press("ENTER");
+delay(500)
+press("GUI r");
+delay(500)
+type("cmd");
+press("ENTER");
+delay(500)
+type("color a");
+press("ENTER");
+delay(500)
+type("START");
+press("ENTER");
+delay(500)
+press("GUI r");
+delay(500)
+type("cmd");
+press("ENTER");
+delay(500)
+type("color a");
+press("ENTER");
+delay(500)
+type("START");
+press("ENTER");
+delay(500)
+press("GUI r");
+delay(500)
+type("cmd");
+press("ENTER");
+delay(500)
+type("tree");
+press("ENTER");
+delay(500)
+press("GUI r");
+delay(500)
+type("cmd");
+press("ENTER");
+delay(500)
+type("tree");
+press("ENTER");
+delay(500)
+press("GUI r");
+delay(500)
+type("cmd");
+press("ENTER");
+delay(500)
+type("tree");
+press("ENTER");
+delay(500)
+press("GUI r");
+delay(500)
+type("cmd");
+press("ENTER");
+delay(500)
+type("tree");
+press("ENTER");
+delay(500)
+press("GUI r");
+delay(500)
+type("cmd");
+press("ENTER");
+delay(500)
+type("tree");
+press("ENTER");
+delay(500)
+press("GUI r");
+delay(500)
+type("cmd");
+press("ENTER");
+delay(500)
+type("tree");
+press("ENTER");
+delay(500)
+press("GUI r");
+delay(500)
+type("cmd");
+press("ENTER");
+delay(500)
+type("tree");
+press("ENTER");
+delay(500)
+press("GUI r");
+delay(500)
+type("cmd");
+press("ENTER");
+delay(500)
+type("tree");
+press("ENTER");
+delay(500)
+press("GUI r");
+delay(500)
+type("cmd");
+press("ENTER");
+delay(500)
+type("tree");
+press("ENTER");
+delay(500)
+press("GUI r");
+delay(500)
+type("cmd");
+press("ENTER");
+delay(500)
+type("tree");
+press("ENTER");
+delay(500)
+press("GUI r");
+delay(500)
+type("cmd");
+press("ENTER");
+delay(500)
+type("tree");
+press("ENTER");
+delay(500)
